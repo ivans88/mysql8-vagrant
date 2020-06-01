a69a60f809e3af668a52a2e2a0b91dcd66a574d6
ivans88 <stojic.ivan@gmail.com>
2020-06-01 16:49:27 +0200

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -5,8 +5,8 @@ export DEBIAN_FRONTEND="noninteractive";
 sudo apt-get update
 sudo apt-get install -y debconf-utils vim curl
 sudo debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-server select mysql-8.0'
-wget https://dev.mysql.com/get/mysql-apt-config_0.8.10-1_all.deb
-sudo -E dpkg -i mysql-apt-config_0.8.10-1_all.deb
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.15-1_all.deb
+sudo -E dpkg -i mysql-apt-config_0.8.15-1_all.deb
 sudo apt-get update
 
 # Install MySQL 8
